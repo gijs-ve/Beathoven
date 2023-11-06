319b6d3b0d263e59fae9b1ecb65ddf8b71e62be4
AdneCode <super-gijs@hotmail.com>
2023-11-06 21:29:06 +0100

    wip: hover KEK

--- src/components/Navbar/NavItem.tsx
+++ src/components/Navbar/NavItem.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export const NavItem = ({ navItem }: { navItem: T_NavItem }) => {
   return (
-    <button className="flex items-center justify-center px-16 py-8 h-4 text-xl border-xl border-2 rounded-xl mx-4 bg-blue-700 border-orange-400 w-[250px]">
+    <button className="flex items-center justify-center px-16 py-8 h-4 text-xl border-xl border-2 rounded-xl mx-4 bg-blue-700 border-orange-400 w-[250px] hover:bg-orange-600">
       <Link href={navItem.href}>{navItem.title}</Link>
     </button>
   );

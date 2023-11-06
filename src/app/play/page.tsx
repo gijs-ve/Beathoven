4a72648ccd685eeca3bcf5cbf017165475d2364a
AdneCode <super-gijs@hotmail.com>
2023-11-06 21:46:09 +0100

    feat: navBar

---
+++ src/app/play/page.tsx
@@ -0,0 +1,9 @@
+export default function Play() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 w-full bg-gray-800">
+      <div className="bg-blueish-100 w-[90%] px-[10%] py-[5%] rounded-xl border-2 border-orange-500">
+        <h1 className="my-8"> Play</h1>
+      </div>
+    </main>
+  );
+}
--- src/components/Navbar/NavItem.tsx
+++ src/components/Navbar/NavItem.tsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 
 export const NavItem = ({ navItem }: { navItem: T_NavItem }) => {
   return (
-    <button className="flex items-center justify-center px-16 py-8 h-4 text-xl border-xl border-2 rounded-xl mx-4 bg-blue-700 border-orange-400 w-[250px] hover:bg-orange-600">
-      <Link href={navItem.href}>{navItem.title}</Link>
-    </button>
+    <Link href={navItem.href}>
+      <button className="flex items-center justify-center px-16 py-8 h-4 text-xl border-xl border-2 rounded-xl mx-4 bg-blue-700 border-orange-400 w-[250px] hover:bg-orange-600">
+        {navItem.title}
+      </button>
+    </Link>
   );
 };

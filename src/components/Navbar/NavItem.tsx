ac7130450fefe8558a908d0a2c75c0e3925d867c
AdneCode <super-gijs@hotmail.com>
2023-11-09 18:55:54 +0100

    feat: link for top left logo

--- src/components/Navbar/NavItem.tsx
+++ src/components/Navbar/NavItem.tsx
@@ -8,9 +8,10 @@ export const NavItem = ({ navItem }: { navItem: T_NavItem }) => {
       <motion.button
         style={{
           height: 68,
+          y: 0,
         }}
-        whileHover={{ height: 76 }}
-        className="flex items-center justify-center px-16 py-8 text-xl border-xl border-2 rounded-xl mx-4 bg-blueish-700 border-blueish-800 w-[250px] hover:bg-blueish-800 hover:drop-shadow-xl"
+        whileHover={{ height: 72, y: -4 }}
+        className="flex items-center justify-center px-16 py-8 text-xl border-2 rounded-xl mx-4 bg-blueish-700 border-blueish-800 w-[250px] hover:bg-blueish-800 hover:border-blueish-900 hover:drop-shadow-xl"
       >
         {navItem.title}
       </motion.button>

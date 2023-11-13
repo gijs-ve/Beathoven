ce2e46ce691326756e9dab4588526d121b0549aa
AdneCode <super-gijs@hotmail.com>
2023-11-13 19:20:28 +0100

    sort: deconstruct

--- src/components/Chatbox/Message.tsx
+++ src/components/Chatbox/Message.tsx
@@ -4,10 +4,10 @@ interface MessageProps {
   message: { text: string; sender: string; time: Date };
 }
 
-const Message: React.FC<MessageProps> = ({ message }) => {
+const Message: React.FC<MessageProps> = ({ message: { sender, text } }) => {
   return (
     <div>
-      <p>{`${message.sender}: ${message.text}`}</p>
+      <p>{`${sender}: ${text}`}</p>
     </div>
   );
 };

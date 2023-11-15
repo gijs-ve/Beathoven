73c9ffba7ae7cfe6effea586556202e2a83d9e88
AdneCode <super-gijs@hotmail.com>
2023-11-15 18:52:50 +0100

    feat: nav

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -3,10 +3,12 @@ import { NavItem } from "./NavItem";
 
 export const Navbar = ({ navItems }: { navItems: T_NavItem[] }) => {
   return (
-    <header className="py-[3%] flex justify-center items-center w-[100%] h-8 bg-gray-800">
-      {navItems.map((navItem) => {
-        return <NavItem key={navItem.href} navItem={navItem} />;
-      })}
+    <header>
+      <nav className="py-[3%] flex justify-center items-center w-[100%] h-8 bg-gray-800">
+        {navItems.map((navItem) => {
+          return <NavItem key={navItem.href} navItem={navItem} />;
+        })}
+      </nav>
     </header>
   );
 };
